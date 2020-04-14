ab7f95e0d0a4811975d9e1088e663195613b56ea
Yarriba Castro <yarribacastro@Yarribas-MacBook-Pro.local>
2020-04-13 19:27:22 -0500

    config

---
+++ config.js
@@ -0,0 +1,9 @@
+const knex = require('knex');
+const config = {
+    client: 'sqlite3',
+    connection: {
+        filename: './data/budget.db3',
+    },
+    useNullAsDefault: true,
+};
+module.exports = knex(config);
